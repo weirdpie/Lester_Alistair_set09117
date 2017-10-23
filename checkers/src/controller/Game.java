5f6754e490140790c8dc7023318034e1e8db9867
weirdpie <alibobo8@hotmial.co.uk>
2017-10-23 20:39:40 +0100

    changed array
     added more classes

---
+++ checkers/src/controller/Game.java
@@ -0,0 +1,10 @@
+package controller;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import controller.Gameboard;
+import java.util.LinkedList;
+import java.util.Scanner;
+public class Game {
+
+}
---
+++ checkers/src/controller/Gameboard.java
@@ -0,0 +1,24 @@
+
+
+package controller;
+
+public class HelloWorld {
+
+    }
+
+    public void printBoard() {
+        System.out.println("  ╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗");
+        System.out.println("  ║║░░░║║░░░║║░░░║║░░░║");
+        System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
+        System.out.println("  ║░░░║║░░░║║░░░║║░░░║║");
+        System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
+        System.out.println("  ║║░░░║║░░░║║░░░║║░░░║");
+        System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
+        System.out.println("  ║░░░║║░░░║║░░░║║░░░║║");
+        System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
+        System.out.println("  ║║░░░║║░░░║║░░░║║░░░║");
+        System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
+        System.out.println("  ║░░░║║░░░║║░░░║║░░░║║");
+        System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
+        System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
+}
