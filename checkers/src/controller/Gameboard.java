8bdcefd606f551b81d4bde33c7222eda3cae318b
weirdpie <alibobo8@hotmial.co.uk>
2017-10-25 11:58:41 +0100

    Added 2d array

--- checkers/src/controller/Gameboard.java
+++ checkers/src/controller/Gameboard.java
@@ -2,7 +2,38 @@
 
 package controller;
 
-public class HelloWorld {
+public class Gameboard {
+    public Gameboard() {
+    }
+
+    public static void main(String[] args) {
+    	 int blackPieces = 12;
+    	    int redPieces = 12;
+    	    final int rows = 8;
+    	    final int columns = 8;
+        int[][] gameboard = new int[][]{
+        	{1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+        	{0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+        	{1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+        	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        	{0, 2, 0, 2, 0, 2, 0, 2, 0, 2},
+        	{2, 0, 2, 0, 2, 0, 2, 0, 2, 0},
+        	{0, 2, 0, 2, 0, 2, 0, 2, 0, 2}};
+        	
+        	
+        System.out.println("yee");
+        showingBoard(gameboard);
+    }
+
+    public static void showingBoard(int[][] x) {
+        for(int row = 0; row < x.length; ++row) {
+            for(int column = 0; column < x[row].length; ++column) {
+                System.out.print(x[row][column]);
+            }
+
+            System.out.println();
+        }
 
     }
 
@@ -20,5 +51,10 @@ public class HelloWorld {
         System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
         System.out.println("  ║░░░║║░░░║║░░░║║░░░║║");
         System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
+        System.out.println("  ║║░░░║║░░░║║░░░║║░░░║");
         System.out.println("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
+        System.out.println("  ║░░░║║░░░║║░░░║║░░░║║");
+        System.out.println("  ╚═══╩═══╩═══╩═══╩═══╩═══╩═══╩═══╝");
+    }
+    
 }
