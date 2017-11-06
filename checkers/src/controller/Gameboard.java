7ed065fd244f22bb8424de05e8ec2f1c7e01313b
Alistair Lester <alibobo8@hotmail.co.uk>
2017-11-06 19:47:26 +0000

    fixed the classes problem
    need to change classes from static

--- checkers/src/controller/Gameboard.java
+++ checkers/src/controller/Gameboard.java
@@ -2,43 +2,36 @@
 
 package controller;
 import java.util.Scanner;
-import java.awt.event.InputEvent;
+
+import model.Model;
+import model.Move;
+import model.piece;
+
 import java.util.*;
-import Model.Move;
-import Model.piece;
-import Model.piece;
+
 
 public class Gameboard {
-    public Gameboard() {
-    }
+	
+	
 
-    public static void main(String[] args) {
-    	 int blackPieces = 12;
-    	    int redPieces = 12;
-    	    final int rows = 8;
-    	    final int columns = 8;
-    
-        int[][] gameboard = new int[][]{
-        	{1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-        	{0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
-        	{1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
-        	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        	{0, 2, 0, 2, 0, 2, 0, 2, 0, 2},
-        	{2, 0, 2, 0, 2, 0, 2, 0, 2, 0},
-        	{0, 2, 0, 2, 0, 2, 0, 2, 0, 2}};
-        	
-        	
-        showingBoard(gameboard);
-     
-    }
     
     
     
 
-    public static void showingBoard(int[][] gameboard ) {
+    public static  void showingBoard( ) {
+    	
+    	 int[][] gameboard = new int[][]{
+ 	       	{1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+ 	       	{0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+ 	       	{1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+ 	       	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+ 	       	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+ 	       	{0, 2, 0, 2, 0, 2, 0, 2, 0, 2},
+ 	       	{2, 0, 2, 0, 2, 0, 2, 0, 2, 0},
+ 	       	{0, 2, 0, 2, 0, 2, 0, 2, 0, 2}};
     	
     	String[] xHeader = {"A", "B", "C", "D","E", "F", "G","H"};
+    	 
     
     	for(int i = 0; i<8;i++) {
     		System.out.print((i + 1) + "\t");
@@ -52,42 +45,63 @@ public class Gameboard {
     		System.out.print(xHeader[k] + "\t");
 
     	}
+   
+
     	
     	System.out.println("\t");
 
     }
-    public void movePiece(int xmove, int ymove,int redPiece, int blackPiece, int Piece) {
-   	 Scanner sc=new Scanner(System.in);
+  
+  
 
-       System.out.println("what peice do you want to move  ");
-      int xorigin = sc.nextInt();
-      int yorigin = sc.nextInt();
-       
-       System.out.print(xorigin);
-       System.out.print(yorigin);
-//       int xorigin = piece.getX();
-//      int yorigin = piece.getY(); 
-//       
-//       
-//       System.out.println("where would you like to move it");
-//     
+    
+
+    public static void  move() {
+    
+  	 Scanner sc=new Scanner(System.in);
+  	 
+  
+      System.out.println("what peice do you want to move  ");
+     int xorigin = sc.nextInt();
+     int yorigin = sc.nextInt();
+      
+      System.out.print(xorigin );
+      System.out.print(yorigin);
+//      int xorigin = piece.getX();
+//     int yorigin = piece.getY(); 
+      
+      
+     System.out.println("where would you like to move it");
+//    
 //		  // Save current position, so we can erase the piece.
-//       
+//      
 //		  // Update the location.
 //		  piece.setX(xorigin + xmove);
 //		
-//      piece.setY(yorigin + ymove);
+//     piece.setY(yorigin + ymove);
 //
 //		  // Remove piece from old position
-//		  gameboard[xorigin][yorigin].clear();
+	controller.showingBoard(gameboard)[xorigin][yorigin].clear();
 //
 //		  // Add it to the new position.
 //		  addPiece(piece);
 //		   NewCoord.close();
 //	       coordOr.close();
-       sc.close();
-   	
+      sc.close();
+  	
 		}
 
+	 public static void main(String[] args) {
+   	 
+//		 model Model = new Model();
+//		 model Move = new Move();
+//		 model piece = new piece();
+
+   	
+   
+      Gameboard.showingBoard();
+      Gameboard.move();
+      
     
-}
+   }
+} 
--- checkers/src/model/Move.java
+++ checkers/src/model/Move.java
@@ -1,4 +1,4 @@
-package Model;
+package model;
 
 public class Move {
 int xorigin;
