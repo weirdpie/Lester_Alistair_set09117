7cb42401f00401e66875d7fd5681d5ee34f28458
weirdpie <alibobo8@hotmial.co.uk>
2017-11-13 19:05:26 +0000

    move validation

--- checkers/src/controller/Game.java
+++ checkers/src/controller/Game.java
@@ -54,84 +54,133 @@ public class Game {
 
 	public void moveChecker() {
 		String origin;
+		String moveCord;
 		
 		Scanner sc = new Scanner(System.in);
 
 
 		System.out.println("what peice do you want to move  ");
-		int xorigin = sc.nextInt();
-		int yorigin = sc.nextInt();
+		origin = sc.next();
+				
+		System.out.print(origin);
 		
+	
 		
-		System.out.print(xorigin);
-		System.out.print(yorigin);
-
-
+		String yorigin = origin.substring(0,1);
+		int xorigin = Integer.parseInt(origin.substring(1,2));
+		int convertedYorigin = vaildate_y(yorigin);
+		xorigin -= 1;
+		
 		System.out.println("where would you like to move it");
-		int xmove = sc.nextInt();
-		int ymove = sc.nextInt();
-		
-		Move move = new Move(xorigin, yorigin, xmove, ymove);
-			
-		int type = gameBoard.getGameboard()[yorigin][xorigin];
-		
-		//model.addMove(move);
-
-		gameBoard.getGameboard()[move.getYorigin()][move.getXorigin()]= 0;
-		gameBoard.getGameboard()[move.getYmove()][move.getXmove()] = type;
-		
-//		String origin = origin.substring(0,1);
+	moveCord = sc.next();
+	
+	String ymove = moveCord.substring(0,1);
+	int xmove = Integer.parseInt(moveCord.substring(1,2));
+	int convertedYmove = vaildate_y(ymove);
+	xmove -=1;
+	
+	System.out.printf(yorigin,convertedYorigin,ymove,convertedYmove,xorigin,xmove);
+	
+	
+	
+//	Move move = new Move(xorigin, yorigin, xmove, ymove);
+//			
+//		int type = gameBoard.getGameboard()[yorigin][xorigin];
+//		
+//		//model.addMove(move);
+//
+//		gameBoard.getGameboard()[move.getYorigin()][move.getXorigin()]= 0;
+//		gameBoard.getGameboard()[move.getYmove()][move.getXmove()] = type;
+//		
+////		String origin = origin.substring(0,1);
 
 		vaildate_y(yorigin);
 		showingBoard();
 		sc.close();
 
 	}
-	
-//	public static Boolean checkInput (int xorigin,int yorigin,int xmove , int ymove ) {
+//	public void moveChecker() {
+//		String origin;
+//		
+//		Scanner sc = new Scanner(System.in);
+//
+//
+//		System.out.println("what peice do you want to move  ");
+//		int xorigin = sc.nextInt();
+//		int yorigin = sc.nextInt();
 //		
 //		
+//		System.out.print(xorigin);
+//		System.out.print(yorigin);
+//
+//
+//		System.out.println("where would you like to move it");
+//		int xmove = sc.nextInt();
+//		int ymove = sc.nextInt();
 //		
+//		Move move = new Move(xorigin, yorigin, xmove, ymove);
+//			
+//		int type = gameBoard.getGameboard()[yorigin][xorigin];
 //		
+//		//model.addMove(move);
+//
+//		gameBoard.getGameboard()[move.getYorigin()][move.getXorigin()]= 0;
+//		gameBoard.getGameboard()[move.getYmove()][move.getXmove()] = type;
 //		
-//		
-//		return null;
-//		
-//		
+////		String origin = origin.substring(0,1);
+//
+//		vaildate_y(yorigin);
+//		showingBoard();
+//		sc.close();
+//
 //	}
-//	
-	public int vaildate_y(int yorigin) {
+	
+	public static Boolean checkInput (String sc) {
 		
-		int ycord = 0;
+		
+		
+		
+		
+		
+		return null;
+		
+		
+	}	
+	public int vaildate_y(String yorigin) {
+		
+   	yorigin = yorigin.toUpperCase();
+		int ycord;
 		
 		switch (yorigin) {
-			case 'A' :
+			case "A" :
 				ycord = 0;
 				break;
-			case 'B' :
+			case "B" :
 				ycord = 1;
 				break;
-			case 'C' :
+			case "C" :
 				ycord = 2;
 				break;
-			case 'D' :
+			case "D" :
 				ycord = 3;
 				break;
-			case 'E' :
+			case "E" :
 				ycord = 4;
 				break;
-			case 'F' :
+			case "F" :
 				ycord = 5;
 				break;
-			case 'G' :
+			case "G" :
 				ycord = 6;
 				break;
-			case 'H' :
+			case "H" :
 				ycord = 7;
 				break;
 				
 				
-				
+			default:
+				ycord = 8;
+				break;
 				
 			
 		}
@@ -149,7 +198,7 @@ public class Game {
 		Game controller = new Game();
 		controller.populate();
 		controller.showingBoard();
-		controller.moveChecker();
+		controller.moveChecker(); 
 
 	}
 }
