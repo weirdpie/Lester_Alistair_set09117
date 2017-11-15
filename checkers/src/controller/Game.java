e921efcc11e66ec1a842bdee8aaf2f17446a6f67
weirdpie <alibobo8@hotmial.co.uk>
2017-11-15 21:15:33 +0000

    input validation

--- checkers/src/controller/Game.java
+++ checkers/src/controller/Game.java
@@ -3,7 +3,6 @@ package controller;
 
 import java.util.Scanner;
 
-
 import model.Model;
 import model.Move;
 import model.Piece;
@@ -13,14 +12,12 @@ import java.util.*;
 
 public class Game {
 
-	//turns
-	//undo re do 
-	// do move validation
-	// add move to list
+	// move checker
+	// taking a peice
+	// undo re do
 	// do jump
 	// jump validation = move vaildation
 	// kings
-	
 
 	Model model = new Model();
 
@@ -31,22 +28,7 @@ public class Game {
 	}
 
 	public void showingBoard() {
-		
-//		while(!gameEnd) {
-//		
-//			if (counter%2 =0 )
-//			{
-//				player1();
-//				player2();
-//			}
-//			if (counter %2 =1) {
-//				player2();
-//				player1();
-//			}
-//			counter ++; 
-//				
-//		}
-		
+
 		String[] xHeader = { "A", "B", "C", "D", "E", "F", "G", "H" };
 
 		for (int i = 0; i < 8; i++) {
@@ -67,86 +49,107 @@ public class Game {
 
 	}
 
-	public void moveChecker(int turn,boolean play) {
-		String origin ="";
-		String moveCord= "";
-		
+	public void moveChecker(int turn, boolean play) {
+		String origin = "";
+		String moveCord = "";
+		boolean error = false;
 		Scanner sc = new Scanner(System.in);
-       while (play==true) {
-		System.out.println("what peice do you want to move  ");
-		origin = sc.next();
+		while (play == true) {
+			
+			System.out.println("what peice do you want to move  ");
+			origin = sc.next();
 
-		System.out.print(origin);
+			System.out.print(origin);
+			
+			if (validateUserInput(origin)) {
+				System.out.println("Please enter a letter and a number form a-h and 1-7 (a3)");
+				error = true;
+			} else {
+				error = false;
+			}
+			while (error);
 
-		String xorigin = origin.substring(0, 1);
-		int yorigin = Integer.parseInt(origin.substring(1, 2));
-		int convertedXorigin = vaildate_x(xorigin);
-		yorigin -= 1;
+			String xorigin = origin.substring(0, 1);
+			int yorigin = Integer.parseInt(origin.substring(1, 2));
+			int convertedXorigin = vaildate_x(xorigin);
+			yorigin -= 1;
 
-		System.out.println("where would you like to move it");
-		moveCord = sc.next();
+			System.out.println("where would you like to move it");
+			moveCord = sc.next();
 
-		String xmove = moveCord.substring(0, 1);
-		int ymove = Integer.parseInt(moveCord.substring(1, 2));
-		int convertedXmove = vaildate_x(xmove);
-		ymove -= 1;
+			if (validateUserInput(moveCord)) {
+				System.out.println("Please enter a letter and a number form a-h and 1-7 (a3)");
+				error = true;
+			} else {
+				error = false;
+			}
+			while (error);
 
-		// System.out.print(yorigin,convertedXorigin,ymove,convertedXmove,xorigin,xmove);
+			String xmove = moveCord.substring(0, 1);
+			int ymove = Integer.parseInt(moveCord.substring(1, 2));
+			int convertedXmove = vaildate_x(xmove);
+			ymove -= 1;
 
-		Move move = new Move(yorigin, convertedXorigin, ymove, convertedXmove);
-		//
-		int type = gameBoard.getGameboard()[convertedXorigin][yorigin];
-		//
-		model.addMove(convertedXorigin, yorigin, convertedXmove, ymove);
-		System.out.print(move+"\n");
-		
-		//model.moves.add(convertedXorigin, yorigin, convertedXmove, ymove);
-		gameBoard.getGameboard()[move.getYorigin()][move.getXorigin()] = 0;
-		gameBoard.getGameboard()[move.getYmove()][move.getXmove()] = type;
-		
-		vaildate_x(xorigin);
-		showingBoard();
-		turn = changeTurn(turn);
-	
-       }
-		
-		
-		 
-//if 1 trys to move into 2 get new user input 
-		//call move moveChecker to make sure user is inputing the right data 
-		//if type any peice = 0 end game and declare the other user the winner.
+			// System.out.print(yorigin,convertedXorigin,ymove,convertedXmove,xorigin,xmove);
+
+			Move move = new Move(yorigin, convertedXorigin, ymove, convertedXmove);
+			//
+			int type = gameBoard.getGameboard()[convertedXorigin][yorigin];
+			//
+			model.addMove(convertedXorigin, yorigin, convertedXmove, ymove);
+			System.out.print(move + "\n");
+
+			// model.moves.add(convertedXorigin, yorigin, convertedXmove, ymove);
+			gameBoard.getGameboard()[move.getYorigin()][move.getXorigin()] = 0;
+			gameBoard.getGameboard()[move.getYmove()][move.getXmove()] = type;
+
+			vaildate_x(xorigin);
+			showingBoard();
+			turn = changeTurn(turn);
+
+		}
+
+		// if 1 trys to move into 2 get new user input
+		// call move moveChecker to make sure user is inputing the right data
+		// if type any peice = 0 end game and declare the other user the winner.
 	}
 
-
 	public static Boolean checkInput(String sc) {
-		//call move moveChecker to make sure user is inputing the right data 
+		// call move moveChecker to make sure user is inputing the right data
 		return null;
 
 	}
-	
+
 	public void checkMove() {
-//		If red piece  can only move +1  +1 , +1 -1 , -1 +1 , -1 -1 i
-//		If black piece can only move -1 -1 , -1 +1 , +1 -1 , +1 +1
-//		else invalid move 
-		// restart players turn and ask them to move again 
+		// If red piece can only move +1 +1 , +1 -1 , -1 +1 , -1 -1 i
+		// If black piece can only move -1 -1 , -1 +1 , +1 -1 , +1 +1
+		// else invalid move
+		// restart players turn and ask them to move again
 	}
-	public void checkTake () {
-//		If red take , black piece +1 +1 empty , black piece +1 -1 empty then take and delete 
-//		If black take , red piece -1 -1 empty , red piece -1 +1 empty then take and delete  
-//		else invalid move 
-		// restart players turn and ask them to move again 
+
+	public void checkTake() {
+		// If red take , black piece +1 +1 empty , black piece +1 -1 empty then take and
+		// delete
+		// If black take , red piece -1 -1 empty , red piece -1 +1 empty then take and
+		// delete
+		// else invalid move
+		// restart players turn and ask them to move again
 	}
+
 	public void king() {
-		//if piece reached 0 or 8 , change peice type to king ( colour ) and end turn 
+		// if piece reached 0 or 8 , change peice type to king ( colour ) and end turn
 	}
+
 	public void checkKing() {
-		//king type can move any way 
+		// king type can move any way
 	}
-	public void undo () {
-		//move list -1 ? 
+
+	public void undo() {
+		// move list -1 ?
 	}
-	public void redo () {
-		//throw list +1 
+
+	public void redo() {
+		// throw list +1
 	}
 
 	public int vaildate_x(String xorigin) {
@@ -183,40 +186,56 @@ public class Game {
 		default:
 			xcord = 8;
 			break;
-			
-			
 
 		}
 		return xcord;
-		
+
 	}
-	public int changeTurn (int turn) {
-		if (turn ==1) {
-			turn=2;
-			System.out.println("Its player 2s turn (white 2's"); 
+
+	public int changeTurn(int turn) {
+		if (turn == 1) {
+			turn = 2;
+			System.out.println("Its player 2s turn (white 2's");
+		} else if (turn == 2) {
+			turn = 1;
+			System.out.println("Its player 1s turn (blacks 1's");
 		}
-		else if (turn==2) {
-			turn=1;
-			System.out.println("Its player 1s turn (blacks 1's"); 
-		}
-		
+
 		return turn;
-		
+
+	}
+
+	public static Boolean validateUserInput(String input) {
+		String xInput = input.substring(0, 1);
+
+		int yInput = Integer.parseInt(input.substring(1, 2));
+
+		boolean xletter = Character.isLetter(xInput.charAt(0));
+
+		if ((!input.isEmpty() || (!xletter))) {
+			return false;
+
+		} else {
+			return true;
+		}
+
 	}
 
 	public static void main(String[] args) {
-		//Scanner sc = new Scanner(System.in);
+		// Scanner sc = new Scanner(System.in);
 		Game controller = new Game();
 		controller.populate();
 		controller.showingBoard();
-		int turn =1 ;
-		boolean play=true;
+		int turn = 1;
+		boolean play = true;
 		do {
-		controller.moveChecker(turn,play);
-		}while (play==true);
-		
-	while (play==false);{
-		System.out.println("the game has ended");
-	}
+			controller.moveChecker(turn, play);
+		} while (play == true);
+
+		while (play == false)
+			;
+		{
+			System.out.println("the game has ended");
+		}
 	}
 }
